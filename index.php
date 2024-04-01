7bbc300543bb6e026c93ece6c0e086790b3c6352
IgorJF <111748228+IgorJF@users.noreply.github.com>
2024-04-01 10:35:20 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Igor Ferraz</title>
+</head>
+
+<body>
+    <div id="navbar">
+        <nav>
+            <ul>
+                <li>Ínício</li>
+                <li>Sobre</li>
+                <li>Currículo</li>
+                <li>Artigos</li>
+                <li>Projetos</li>
+                <li>Diário</li>
+            </ul>
+        </nav>
+    </div>
+
+    <div>
+        <section>
+
+        </section>
+    </div>
+
+    <div>
+        <footer>
+            <p>© Igor Jorge Ferraz</p>
+        </footer>
+    </div>
+
+</body>
+
+</html>
